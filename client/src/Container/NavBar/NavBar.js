aa3ad12d5a49629b2b0cd95fbfc1d3258f29ff93
KarenDerkach <86269842+KarenDerkach@users.noreply.github.com>
2022-02-05 08:44:44 -0300

    responsive home

--- client/src/Container/NavBar/NavBar.js
+++ client/src/Container/NavBar/NavBar.js
@@ -1,7 +1,11 @@
 import React, {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { BiMenu} from 'react-icons/bi';
 import {changeLanguage} from '../../react-redux/action'
-import { Link} from 'react-router-dom'
+// import { Link} from 'react-router-dom'
+
+import ES from '../../assets/Home/ES.png'
+import EN from '../../assets/Home/EN.png'
 import './NavBar.css';
 
 
@@ -28,16 +32,22 @@ export default function NavBar() {
         <div >
             <div>
             <button onClick={(e) => handleChangeLang(e)} className="buttonLang">
-					{language === 'ES' ? 'EN' : 'ES'}
+					{language === 'ES' ? <img src={EN} alt='U.S' className="flat"/> : <img src={ES} alt='ES' className="flat"/>}
 				</button>
             </div>
-            {/* <nav className="navbar">
-                <Link to='/' className='i-home' textDecoration="none" >{language === 'EN' ? 'Home' : 'Principal' }</Link> 
-                <Link to='/AboutMe' className='i-about'  > {language === 'EN' ? 'About Me' : 'Acerca de Mi' }</Link> 
-                <Link to='/Portfolio' className='i-portfolio'  > {language === 'EN' ? 'Projects' : 'Proyectos' } </Link> 
-                <Link to='/Contact' className='i-contact' > {language === 'EN' ? 'Contact' : 'Contacto' } </Link>
-            </nav> */}
-            <nav className="navbar" class="navbar navbar-light ">
+            <nav className="navbar">
+                <ul className="menu-items">
+                <li><a href='/' className='i-home'  >{language === 'EN' ? 'Home' : 'Principal' }</a> </li>
+                <li> <a href='/AboutMe' className='i-about'  > {language === 'EN' ? 'About Me' : 'Acerca de Mi' }</a> </li>
+                <li> <a href='/Portfolio' className='i-portfolio'  > {language === 'EN' ? 'Projects' : 'Proyectos' } </a> </li>
+                <li>  <a href='/Contact' className='i-contact' > {language === 'EN' ? 'Contact' : 'Contacto' } </a></li>
+                </ul>
+                <div className="btn-menu-mobile">
+                    <BiMenu />
+                </div>
+             
+            </nav>
+            {/* <nav className="navbar" class="navbar navbar-light ">
   <div  class="container-fluid">
     <Link to='/'  class="navbar-brand"className='i-home' >{language === 'EN' ? 'Home' : 'Principal' }</Link>
     <Link to='/AboutMe' class="navbar-brand" className='i-about'  > {language === 'EN' ? 'About Me' : 'Acerca de Mi' }</Link> 
@@ -45,7 +55,7 @@ export default function NavBar() {
     <Link to='/Contact'  class="navbar-brand" className='i-contact' > {language === 'EN' ? 'Contact' : 'Contacto' } </Link>
 
   </div>
-</nav>
+</nav> */}
    
         </div>
 
--- client/src/Container/Projects/Projects.js
+++ client/src/Container/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ModalDetails from './ModalDetails';
+// import ModalDetails from './ModalDetails';
 import './Projects.css'
 // import foodarity from '../../assets/Projects/foodarity.jpg'
 
@@ -14,23 +14,40 @@ export default function Projects({project}) {
 
     return (
         
-        <div class="card" style={{width: 400 }}>
-        <button type="button" className='buttonModal' data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <img src={project.image} class="card-img-top" alt="imagen"/>
-            </button>
-                {/* <div class="card-body">
-                    <h5 class="card-title"><b>{project.title}</b></h5>
-                    <p class="card-text">{project.description}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><b>Tools:</b> {project.tools}</li>
-                    <li class="list-group-item"><b>Project deploy:</b> <a href={project.link}>HERE!</a></li>
-                </ul> */}
+        // <div class="card" style={{width: 400 }}>
+        // <button type="button" className='buttonModal' data-bs-toggle="modal" data-bs-target="#exampleModal">
+        //     <img src={project.image} class="card-img-top" alt="imagen"/>
+        //     </button>
+        //         <div class="card-body">
+        //             <h5 class="card-title"><b>{project.title}</b></h5>
+        //             <p class="card-text">{project.description}</p>
+        //         </div>
+        //         <ul class="list-group list-group-flush">
+        //             <li class="list-group-item"><b>Tools:</b> {project.tools}</li>
+        //             <li class="list-group-item"><b>Project deploy:</b> <a href={project.link}>HERE!</a></li>
+        //         </ul>
         
-              <ModalDetails  project={project}/>
+        //       <ModalDetails  project={project}/>
             
+        // </div>
+        <div class="container">
+        <div class="card">
+            <div class="face face1">
+                <div class="content">
+                    <img src={project.image} alt='imgProject'/>
+                    <h3>{project.title}</h3>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content">
+                    <p>{project.description}</p>
+                    {/* <p>{project.tools}</p> */}
+                        <a href={project.code}>ver codigo</a>
+                        <a href={project.link}>ver página</a>
+                </div>
+            </div>
         </div>
-
+        </div>
     
     )
 }
